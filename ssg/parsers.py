67cba8c4bbfc5a8b2d03e750f3ce266cea1a04ce
nitishr06@gmail.com <nitish.a.ranjan@accenture.com>
2020-09-02 17:30:06 +0530

    The Parser Class

---
+++ ssg/parsers.py
@@ -0,0 +1,37 @@
+from pathlib import Path
+from typing import List
+import shutil
+
+class Parse:
+    extensions: List[str]= []
+
+    def valid_extension(self,extension):
+        return extension in self.extensions
+       
+    def parse(self,path,source,dest):
+        if NotImplementedError:
+            raise NotImplementedError("Method not implemented!")
+
+    def read(self,path):
+        with open(path,'r') as file:
+            return file.read()
+    
+    def write(self,path,dest,content,ext = ".html"):
+        full_path = dest / path.with_suffix(ext).name
+        with open(full_path,'w') as file:
+            file.write(content)
+    
+    def copy(self,path,source,dest):
+        shutil.copy2(path,dest/source)
+
+    
+class ResourceParser(Parse):
+
+    extensions = [".jpg", ".png", ".gif", ".css",".html"]
+
+    def parse(self,path,source,dest):
+        super().copy(path,source,dest)
+
+
+
+
--- ssg/site.py
+++ ssg/site.py
@@ -2,9 +2,10 @@ from pathlib import Path
 
 class Site:
 
-    def __init__(self,source,dest):
+    def __init__(self,source,dest,parsers = None):
         self.source = Path(source)
         self.dest = Path(dest)
+        self.parsers = []
 
     def create_dir(self, path):
         directory = self.dest/ path.relative_to(self.source)
@@ -15,3 +16,19 @@ class Site:
         for path in self.source.rglob("*"):
             if path.is_dir():
                 self.create_dir(path)
+            elif path.is_file():
+                self.run_parser(path)
+    
+    def load_parser(self,extension):
+        for parser in self.parsers:
+            if valid_extension(parser):
+                return parser
+    
+    def run_parser(self,path):
+        parser =  load_parser(path.suffix)
+
+        if(parser!= None):
+            parse(path,self.source,self.dest)
+        else:
+            print("Not Implemented!!")
+        
